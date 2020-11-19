a02123373ebc222f48b46c8ce163394fb7e0354b
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-11-19 22:45:56 +1030

    Create Simple_URL_Storer.cs

---
+++ Simple_URL_Storer.cs
@@ -0,0 +1,22 @@
+using System;
+
+/*
+Plan:
+- Ask for name.
+- Ask for URL.
+- Analyse category (Domain name).
+- Store both name and URL in a persistent text file.
+ */
+
+
+namespace Simple_URL_Storer
+{
+    class Simple_URL_Storer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: ");
+            Console.WriteLine("URL: ");
+        }
+    }
+}
