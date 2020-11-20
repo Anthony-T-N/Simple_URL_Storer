930d4dca04fd8cf816d955bced2dac460b50b3f0
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-11-20 12:05:55 +1030

    Update Simple_URL_Storer.cs

--- Simple_URL_Storer.cs
+++ Simple_URL_Storer.cs
@@ -1,11 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 /*
 Plan:
-- Ask for name.
+- Ask for name/title.
 - Ask for URL.
-- Analyse category (Domain name).
-- Store both name and URL in a persistent text file.
+- Optiona: Analyse category (Domain name).
+- Automatically add date above record.
+- Store both name and URL stored as a record in a persistent text file.
+- Each record is divided by space for easy viewing.
+
+Record Example:
+
+===<Category>===
+
+XX/XX/2020
+<Title>
+<URL>
+
+XX/XX/2020
+<Title>
+<URL>
+
  */
 
 
@@ -15,8 +31,26 @@ namespace Simple_URL_Storer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Name: ");
-            Console.WriteLine("URL: ");
+            Simple_URL_Storer main_program = new Simple_URL_Storer();
+            main_program.input_intake();
+        }
+        public void input_intake()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("URL: ");
+            string url = Console.ReadLine();
+
+            List<string> record = new List<string>() {
+                DateTime.Now.ToString("yyyy-MM-dd"), 
+                name, 
+                url
+            };
+            for (int i = 0; i <= record.Count - 1; i++)
+            {
+                Console.WriteLine(record[i]);
+            }
+
         }
     }
 }
